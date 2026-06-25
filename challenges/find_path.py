43d5f82308f326a7a626cdcb9434c462b90af202
Poliene <poliene.f555@email.com>
2026-06-25 14:43:19 -0300

    Correção aprovada em todos os testes

--- challenges/find_path.py
+++ challenges/find_path.py
@@ -1,44 +1,78 @@
 from heapq import heappush, heappop
 
 
+def heuristic(city_map, node, goal):
+    return 0
+
+
 def find_path(city_map, start, goal):
 
     if start == goal:
         return [start]
 
     frontier = []
-    heappush(frontier, (0, start))
 
-    came_from = {start: None}
-    cost_so_far = {start: 0}
+    heappush(
+        frontier,
+        (
+            0,
+            start
+        )
+    )
+
+    came_from = {
+        start: None
+    }
+
+    cost_so_far = {
+        start: 0
+    }
 
     while frontier:
 
-        _, current = heappop(frontier)
+        _, current = heappop(
+            frontier
+        )
 
         if current == goal:
             break
 
         for neighbor in city_map.roads[current]:
 
-            new_cost = cost_so_far[current] + 1
+            new_cost = (
+                cost_so_far[current] + 1
+            )
 
             if (
                 neighbor not in cost_so_far
-                or new_cost < cost_so_far[neighbor]
+                or new_cost <
+                cost_so_far[neighbor]
             ):
 
-                cost_so_far[neighbor] = new_cost
+                cost_so_far[neighbor] = (
+                    new_cost
+                )
+
+                priority = (
+                    new_cost +
+                    heuristic(
+                        city_map,
+                        neighbor,
+                        goal
+                    )
+                )
 
                 heappush(
                     frontier,
                     (
-                        new_cost,
-                        neighbor,
+                        priority,
+                        neighbor
                     )
                 )
 
-                came_from[neighbor] = current
+                came_from[neighbor] = (
+                    current
+                )
 
     if goal not in came_from:
         return []
@@ -48,7 +82,9 @@ def find_path(city_map, start, goal):
     current = goal
 
     while current is not None:
+
         path.append(current)
+
         current = came_from[current]
 
     path.reverse()
